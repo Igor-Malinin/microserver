abcc54274ebf6f8aeabad0b448b3cde92625edd7
GauraNitay <vip.igorka@gmail.com>
2023-10-31 23:18:03 +0400

    Initial

--- src/main/java/com/server/microserver/MicroserverApplication.java
+++ src/main/java/com/server/microserver/MicroserverApplication.java
@@ -2,12 +2,15 @@ package com.server.microserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableConfigServer
 public class MicroserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserverApplication.class, args);
+    }
 
 }
